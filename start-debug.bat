762ad4ca975e5e34e821111582e1cbd6bcd72356
Jonathan Bruce <jbruce77@gmail.com>
2018-09-06 20:28:02 -0400

    Update location

--- start-debug.bat
+++ start-debug.bat
@@ -1,4 +1,4 @@
-start "MongoDb" "C:\Program Files\MongoDB\Server\3.4\bin\runmongod.cmd"
+start "MongoDb" "C:\Program Files\MongoDB\Server\runmongod.cmd"
 start "N.Blog API" cmd /c "cd api && nodemon"
 start "N.Blog Client" cmd /c "cd client && npm start"
 exit
